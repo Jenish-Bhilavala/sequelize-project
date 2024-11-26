faae9e1f16653e68c7ee51d1636d598079d23c78
Jenish Bhilavala <jenishbhilavala.shivinfotech@gmail.com>
2024-11-26 13:25:36 +0530

    add prettierrc

--- app/routes/routers/userRoute.js
+++ app/routes/routers/userRoute.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userModel = require("../../controllers/userController");
+const userModel = require('../../controllers/userController');
 
-router.get("/", userModel.getUser);
+router.get('/', userModel.getUser);
 
 module.exports = router;
